63d826efd4e4a13c529414e43dbb43592ea68341
zwj4031 <dnyl@qq.com>
2023-07-23 19:23:16 +0800

    add minireg

--- x64/x/ipxefm/ipxefm-BTServer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-mode con cols=50 lines=4
-title=BTServer
-
-cd /d %~dp0
-if exist %temp%\qBittorrent\qbittorrent.exe start "" %temp%\qBittorrent\qbittorrent.exe
-start "" %~dp0app\inject\default\btx64.exe
-exit
